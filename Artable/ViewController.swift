a90b40c6bf7320e3d1720b213b434d6f07f5d051
Jonathan Go <jogo88@gmail.com>
2019-07-05 09:52:28 +0800

    Initial Commit

---
+++ Artable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Artable
+//
+//  Created by Jonathan Go on 2019/07/05.
+//  Copyright © 2019 Appdelight. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
